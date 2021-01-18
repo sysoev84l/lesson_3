fa86d6642c0623aa213ea5eca001e7c299202dc2
sysoev84l <sysoev84l@sysoev84l.ga>
2021-01-19 01:29:54 +0300

    lesson_3

--- git_push.sh
+++ git_push.sh
@@ -2,6 +2,7 @@ CURRENT=`pwd`
 BASENAME=`basename "$CURRENT"`
 
 #comment="lesson_3"
+notify-send "git coomit and push $BASENAME"
 git add .
 git commit -m $BASENAME
 git push -u origin main
---
+++ push.sh
@@ -0,0 +1,8 @@
+CURRENT=`pwd`
+BASENAME=`basename "$CURRENT"`
+
+#comment="lesson_3"
+#git add .
+echo $CURRENT
+echo $BASENAME
+notify-send $BASENAME
